1e04cf6484ecd3bf3436c96ca1cb9e9d221f4090
okayu-331 <kuma.fuyuno@gmail.com>
2020-10-22 19:06:35 +0900

    Action Cable実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,7 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-      redirect_to room_messages_path(@room)
+      ActionCable.server.broadcast 'message_channel', {name: current_user.name, content: @message, date: l(@message.created_at)}
     else
       @messages = @room.messages.includes(:user)
       render :index
