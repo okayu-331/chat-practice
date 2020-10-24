8b2df38863625ec1ca683db1ab57990784d7ddc2
okayu-331 <kuma.fuyuno@gmail.com>
2020-10-24 19:59:29 +0900

    メッセージ投稿時の左右出し分け

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -10,7 +10,24 @@ consumer.subscriptions.create("MessageChannel", {
   },
 
   received(data) {
-    const html = `
+    const myMessage = `
+      <div class="my-message">
+        <div class="upper-message">
+          <div class="message-user">
+            ${data.name}
+          </div>
+          <div class="message-date">
+            ${data.date}
+          </div>
+        </div>
+        <div class="lower-message">
+          <div class="message-content">
+            <p>${data.content.content}</p>
+          </div>
+        </div>
+      </div>`;
+
+    const othersMessage = `
       <div class="message">
         <div class="upper-message">
           <div class="message-user">
@@ -26,9 +43,17 @@ consumer.subscriptions.create("MessageChannel", {
           </div>
         </div>
       </div>`;
+
     const messages = document.getElementById('messages');
+    const currentUserId = parseInt(messages.dataset.currentUser, 10);
     const newMessage = document.getElementById('message_content');
-    messages.insertAdjacentHTML('beforeend', html);
+
+    if(currentUserId === data.content.user_id) {
+      messages.insertAdjacentHTML('beforeend', myMessage);
+    } else {
+      messages.insertAdjacentHTML('beforeend', othersMessage);
+    }
+    
     messages.scrollTop = messages.scrollHeight;
     newMessage.value = '';
   }
